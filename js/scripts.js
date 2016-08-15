9863c24001ef925afceaecc6190fece1ad7aec2d
Francis Prus and Sheena Nickerson <sheena@>
2016-08-15 11:03:35 -0700

    dry up the js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,11 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var inputs = ["person", "person2", "animal", "exclamation", "verb", "noun"];
+    inputs.forEach(function(input) {
+      var userInput = $("input#" + input).val();
+      $("." + input).text(userInput);
+    });
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
 
     $("#story").show();
 
